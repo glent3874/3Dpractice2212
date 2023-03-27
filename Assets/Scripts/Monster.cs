88a65f38bdb0c7a79651671da1e3a544d746e535
glent3874 <66166900+glent3874@users.noreply.github.com>
2023-03-27 16:20:45 +0800

    3/27
    註解

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 敵人AI
+/// </summary>
 public class Monster : AYEMonster<壞人行為>
 {
-	[SerializeField] GameObject[] 可走的點 = new GameObject[0];
+    #region 欄位
+    [SerializeField] GameObject[] 可走的點 = new GameObject[0];			//AI要前往的點
 	[SerializeField] Transform 眼睛 = null;
 	[SerializeField] LayerMask 會使視線受阻的圖層;
 	[SerializeField] float 視線距離 = 8f;
@@ -19,26 +23,32 @@ public class Monster : AYEMonster<壞人行為>
 	bool 到處看看的好奇心 = true;
 	float 獵殺時間 = 0f;
 	Vector3 最後見到玩家的位置 = Vector3.zero;
+    #endregion
 
-	private void Awake()
+    #region 事件
+    private void Awake()
 	{
+		//登錄狀態
 		AddStatus(壞人行為.待機, 待機, 刷新待機, 離開待機);
 		AddStatus(壞人行為.巡邏, 巡邏, 刷新巡邏, 離開巡邏);
 		AddStatus(壞人行為.見人, 見人, 刷新見人, 離開見人);
 		AddStatus(壞人行為.追人, 追人, 刷新追人, 離開追人);
 		AddStatus(壞人行為.攻擊, 攻擊, 刷新攻擊, 離開攻擊);
 
-		可走的點 = GameObject.FindGameObjectsWithTag("AIPoint");
+		可走的點 = GameObject.FindGameObjectsWithTag("AIPoint");			//抓可以走的點
 	}
+    #endregion
 
-	#region 待機
-	void 待機()
+    #region 狀態
+    #region 待機
+    void 待機()
 	{
 		到處看看的好奇心 = true;
 	}
 
 	void 刷新待機()
 	{
+		//待機一段時間就巡邏
 		if (IsTime(Random.Range(1f, 4f)))
 		{
 			status = 壞人行為.巡邏;
@@ -102,7 +112,7 @@ public class Monster : AYEMonster<壞人行為>
 
 	void 刷新見人()
 	{
-		if (statusTime > 1f)
+		if (statusTime > 0.7f)
 		{
 			status = 壞人行為.追人;
 		}
@@ -123,12 +133,17 @@ public class Monster : AYEMonster<壞人行為>
 
 	void 刷新追人()
 	{
+		//當正在追人就不斷刷新要前往的位置 即玩家的位置
+		//若還能看見敵人就持續注意力
+		//脫離視線一段時間(注意力) 就會中斷刷新位置
+		//達成AI推測玩家轉彎之後去向的效果
 		if (獵殺時間 > 0f)
 			最後見到玩家的位置 = 我的敵人.position;
 		if (CanSee(眼睛.position, 我的敵人.position, 會使視線受阻的圖層))
 			獵殺時間 = 注意力;
 		else
 			獵殺時間 -= Time.deltaTime;
+
 		Way(最後見到玩家的位置);
 
 		if (Close(最後見到玩家的位置, true, 攻擊距離))
@@ -142,6 +157,7 @@ public class Monster : AYEMonster<壞人行為>
 				status = 壞人行為.待機;
 			}
 		}
+
 		if (statusTime > 30f)
 			status = 壞人行為.待機;
 	}
@@ -178,16 +194,21 @@ public class Monster : AYEMonster<壞人行為>
 	{
 		扇形攻擊(攻擊距離, 攻擊角度, 普通攻擊力);
 	}
-	#endregion
+    #endregion
+    #endregion
 
-	#region 功能
-	void 扇形攻擊(float 距離,float 角度,float 攻擊力)
+    #region 功能
+    void 扇形攻擊(float 距離,float 角度,float 攻擊力)
 	{
+		//捕捉周圍物件的碰撞器
 		Collider[] 周邊的碰撞器 = Physics.OverlapSphere(transform.TransformPoint(0f, 0f, -1f), 距離 + 1f); ;
 		for (int i = 0; i < 周邊的碰撞器.Length; i++)
 		{
+			//如果是自己身上的就跳過
 			if (周邊的碰撞器[i].transform.GetInstanceID() == this.transform.GetInstanceID())
 				continue;
+
+			//計算此物件的位置是否在我的攻擊範圍內
 			Vector3 c = 周邊的碰撞器[i].transform.position;
 			c.y = this.transform.position.y;
 			Vector3 ab = this.transform.forward;
@@ -195,6 +216,7 @@ public class Monster : AYEMonster<壞人行為>
 			float 夾角 = Vector3.Angle(ab, ac);
 			if (夾角 < 角度 * 0.5f)
 			{
+				//如果此物件身上有生命模塊 即可扣血 就使受傷
 				生命模塊 可能的生命模塊 = 周邊的碰撞器[i].gameObject.GetComponent<生命模塊>();
 				if (可能的生命模塊 != null)
 				{
@@ -206,6 +228,7 @@ public class Monster : AYEMonster<壞人行為>
 	#endregion
 }
 
+#region 結構
 public enum 壞人行為
 {
     待機,
@@ -214,3 +237,4 @@ public enum 壞人行為
 	追人,
 	攻擊,
 }
+#endregion
--- Assets/Scripts/生命模塊.cs
+++ Assets/Scripts/生命模塊.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// 生命模組 萬有血條
+/// </summary>
 public class 生命模塊 : MonoBehaviour
 {
+    #region 欄位
     [SerializeField] float maxHp = 10f;
-	[SerializeField] Image 血條 = null; 
+	[SerializeField] Image 血條 = null;
 
     float hp
 	{
@@ -21,13 +25,17 @@ public class 生命模塊 : MonoBehaviour
 		}
 	}
 	float _hp = 0f;
+    #endregion
 
-	private void Start()
+    #region 事件
+    private void Start()
 	{
 		hp = maxHp;
 	}
+    #endregion
 
-	public void 受傷(float 受到的攻擊力, Vector3 對方的座標)
+    #region 方法
+    public void 受傷(float 受到的攻擊力, Vector3 對方的座標)
 	{
 		hp -= 受到的攻擊力;
 
@@ -44,10 +52,13 @@ public class 生命模塊 : MonoBehaviour
 			this.gameObject.SendMessage("Die", 本次傷害, SendMessageOptions.DontRequireReceiver);
 		}
 	}
+    #endregion
 }
 
+#region 結構
 public struct 傷害包
 {
 	public float 攻擊力;
 	public Vector3 攻擊位置;
 }
+#endregion
